cac37a3ca34f20e1197248afec08c91c810007e0
Krisli Dimo <krislidimo@gmail.com>
2019-09-23 23:27:25 -0500

    added created_at, last_modified fields to Note

--- notes/models.py
+++ notes/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from uuid import uuid4
 
+from django.contrib.auth.models import User # Djangos built in user model
+
 # Create your models here.
 
 class Note(models.Model):
@@ -11,3 +13,8 @@ class Note(models.Model):
 	)
 	title = models.CharField(max_length=200)
 	content = models.TextField(blank=True)
+	created_at = models.DateTimeField(auto_now_add=True) # auto_now_add only sets on create
+	last_modified = models.DateTimeField(auto_now=True) # auto_now sets on create & update
+
+class PersonalNote(Note):   # Inherits from Note!
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
